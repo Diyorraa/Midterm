1f443f562cb9bc8522c53b63719346dfa0483abd
flo-tech <florian.vanier@epitech.eu>
2023-04-21 10:23:10 +0000

    feat(image): create a mode image that generate an image with a description

--- chatgptAPI.py
+++ chatgptAPI.py
@@ -5,17 +5,41 @@ import openai
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 print("If you want to quit the chat press 'q' or 'quit'")
+print("Enter 'chat' to chat and 'image' to generate image")
+print("By default you are in chat mode")
 
-while True:
-    orig = input("You: ")
-    if orig == "quit" or orig == "q":
-        break
+def GenerateImage():
+    userInput = input("Give a description of the desired image : ")
+    if userInput == "quit" or userInput == "q":
+        return 0
+    if userInput == "chat":
+        return Chat()
+    img = openai.Image.create(
+        prompt=userInput,
+        n=2,
+        size="1024x1024"
+    )
+    print("url : ", img.data[0].url)
+    return GenerateImage()
+
+
+def Chat():
+    userInput = input("You : ")
+    if userInput == "quit" or userInput == "q":
+        return 0
+    if userInput == "image":
+        return GenerateImage()
     # Do the api call
     completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "user", "content": orig}
+            {"role": "user", "content": userInput}
         ]
-    )
+    )    
     # Print the answer
     print("ChatGpt: ", completion.choices[0].message.content)
+    return Chat()
+
+
+# Start the program
+Chat()
