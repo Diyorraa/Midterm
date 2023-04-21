27259db1932b5feed21ad9907dd6b2a5f3b810e6
flo-tech <florian.vanier@epitech.eu>
2023-04-21 09:55:35 +0000

    feat(chat): put comment in the code

--- chatgptAPI.py
+++ chatgptAPI.py
@@ -1,17 +1,21 @@
 import os
 import openai
 
+# Get the API key from Environment Variables
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+print("If you want to quit the chat press 'q' or 'quit'")
 
 while True:
     orig = input("You: ")
     if orig == "quit" or orig == "q":
         break
+    # Do the api call
     completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
             {"role": "user", "content": orig}
         ]
     )
+    # Print the answer
     print("ChatGpt: ", completion.choices[0].message.content)
